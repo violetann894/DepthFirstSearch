a6b99a24787b66e45d7910729d814e3fa7104e67
violetann894 <rachelhuss2976@gmail.com>
2025-03-17 15:34:32 -0400

    implemented dfs and dfsvisit

--- ProgrammingProject2.py
+++ ProgrammingProject2.py
@@ -1,4 +1,77 @@
-import math
+class Node:
+    """The constructor for the Node class creates a new Node object that holds information about the current state
+        of the Node.
+        key - The data that the Node needs to hold
+        next - The pointer to the next object, starts as none because nothing is connected to a newly created Node
+        prev - The pointer to the previous object, starts as none because nothing is connected to a newly created node
+        """
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+        self.prev = None
+
+
+class LinkedList:
+    """The constructor for the LinkedList class creates a new LinkedList object.
+        head - The Node at the beginning of the list"""
+
+    def __init__(self, head):
+        self.head = head
+
+    """listSearch accepts a key value for a Node in the LinkedList and returns if the Node was found.
+        k - The key value for the desired Node
+
+        Return x - The Node with the key value given, returns none if the Node was not found"""
+
+    def listSearch(L, k):
+        x = L.head
+        while x is not None and x.key != k:
+            x = x.next
+        return x
+
+    """listPrepend accepts a key value for a Node, creates a new Node object and adds it to the beginning of the 
+        linked list.
+        k - The key value for the new head Node"""
+
+    def listPrepend(L, k):
+        x = Node(k)
+        x.next = L.head
+        x.prev = None
+        if L.head is not None:
+            L.head.prev = x
+        L.head = x
+
+    """listInsert inserts a Node after a chosen Node.
+        x - The Node to be inserted after y
+        y - The Node that x will be inserted after"""
+
+    def listInsert(L, x, y):
+        x.next = y.next
+        x.prev = y
+        if y.next is not None:
+            y.next.prev = x
+        y.next = x
+
+    """listDelete deletes the desired node in the linked list.
+        x - The Node object to be deleted from the list"""
+
+    def listDelete(L, x):
+        if x.prev is not None:
+            x.prev.next = x.next
+        else:
+            L.head = x.next
+            if x.next is not None:
+                x.next.prev = x.prev
+
+    """printLinkedList prints out the key values of the linked list, with the value at the top being the 
+        key value of the head of the list."""
+
+    def printLinkedList(L):
+        x = L.head
+        while x is not None:
+            print(x.key)
+            x = x.next
 
 class Graph:
     """Creates an empty adjacency list called aList. This function uses a dictionary. You will be able to access the
@@ -15,6 +88,11 @@ class Graph:
 
         for e in E:
             self.addEdge(e[0], e[1], directed)
+
+        self.color = []
+        self.pi = []
+        self.d = []
+        self.f = []
 
     """Creates an edge. If directed is true, only one direction will be added. If not, both directions will be added
         v1 - The first vertex that makes up the edge
@@ -44,39 +122,67 @@ class Graph:
     def degree(self, v):
         return len(self.aList[v])
 
-    def DFS(self, s):
-        for u in s:
-            u.color = "white"
-            u.pi = None
+    """Runs Depth First Search on the Graph object using s as the source
+        s - Source vertex for DFS"""
 
-        time = 0
-        for u in s:
-            if u.color == "white":
+    def DFS(self, s):
+
+        """This sets up the lists for the DFS search"""
+        for u in range(len(self.aList)):
+            self.color.append("white")
+            self.pi.append(None)
+            self.d.append(0)
+            self.f.append(0)
+
+        """This actually begins the DFS algorithm"""
+        self.time = 0
+        for u in range(len(self.aList)):
+            if self.color[u] == "white":
                 self.DFSVisit(u)
 
+    """Vists the vertex and explores the connected vertices to see where the algorithm can move next"""
 
     def DFSVisit(self, u):
-        time = time + 1
-        u.d = time
-        u.color = "grey"
+        self.time += 1
+        self.d[u] = self.time
+        self.color[u] = "grey"
         for v in self.aList[u]:
-            if v.color == "white":
-                v.pi = u
+            if self.color[v] == "white":
+                self.pi[v] = u
                 self.DFSVisit(v)
-        u.color = "black"
-        time = time + 1
-        u.f = time
+        self.color[u] = "black"
+        self.time += 1
+        self.f[u] = self.time
 
+    """Runs DFS on the list and then creates a linked list of objects sorted in topological order
+    Returns: """
     def TopologicalSort(self):
 
 
-    def Transpose(self):
 
+    """"""
+    #def Transpose(self):
     ##Implement a Graph Transpose to be used in the Strongly Connected Components function
 
-    def SCC(self):
 
 
-##Implement Strongly Connected Components from the psuedocode given in the slides
+    """"""
+    #def SCC(self):
+    ##Implement Strongly Connected Components from the psuedocode given in the slides
+
+
 
 if __name__ == "__main__":
+    V = [0, 1, 2, 3]
+    E = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)]
+
+    graph = Graph(V, E, False)
+    graph.printGraph()
+
+    print()
+
+    graph.DFS(1)
+    print(graph.color)
+    print(graph.d)
+    print(graph.f)
+    print(graph.pi)
